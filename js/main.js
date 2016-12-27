6418d7526dd729cdef5f9a2a50833175ac049e59
Gabriel R Morales <grmorale@gmail.com>
2016-12-26 20:10:13 -0800

    Update javascript for functionality

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-/*As of now, this currently needs the timer to be functioning properly.
-Also needs to start a break timer once the main timer hits 0
-*/
+
 var timerOff=true;
+var interval;
+var counter=0;
+var isBreak=false;
 //Break value controls
 $("#addBreak").click(function(){
 	var break_value=$("#break").html();
@@ -16,14 +17,16 @@ $("#subtractBreak").click(function(){
 });
 //Session value controls
 $("#addSession").click(function(){
-	var session_value=$("#session").html();
-	
+	if (isBreak==false){
+	var session_value=$("#session").html();	
 	$("#session").html(parseInt(session_value)+1);
 	if (timerOff==true){
 	$("#minutes").html($("#session").html());
 }
+}
 });
 $("#subtractSession").click(function(){
+	if (isBreak==false){
 	var session_value=$("#session").html();
 	if (parseInt(session_value)>1){
 	$("#session").html(parseInt(session_value)-1);
@@ -31,13 +34,12 @@ $("#subtractSession").click(function(){
 	$("#minutes").html($("#session").html());
 	}
 }
+}
 });
 
 //Timer controls
 
-var interval;
-var counter=0;
-var isBreak=false;
+
 $("#timer").click(function(){
 	var minutes=$("#minutes").html();
 	var seconds=$("#seconds").html();
@@ -49,20 +51,23 @@ function countdown(){
 	var minutes=$("#minutes").html();
 	seconds=parseInt(seconds);
 	minutes=parseInt(minutes);
-	
+	/*console.log("Seconds: "+seconds);
+	console.log("Minutes: "+minutes);
+	console.log("break: "+isBreak);*/
 	$("#seconds").html(seconds-=1);
 	//Set Break value
 	if (seconds==0&&minutes==0){
 		if (isBreak==false){
-			alert("Your break has started!");
+		alert("Your break has started!");
 		var break_value=$("#break").html();
 		$("#session_title").html("Break");
 		$("#minutes").html(break_value-1);
 		$("#seconds").html(59);
 		isBreak=true;
-		}
+			}
 		else {
-			window.location.reload();
+		alert("break over");
+		window.location.reload();
 		/*$("#session_title").html("Session");
 		countdownOff();
 		alert("Your break is up. Back to work!");
